b5a46c550b47e0b03ad7c072caa2b9bb386f6371
Miguel Zapata <zpmaepp@gmail.com>
2024-07-02 13:45:26 -0500

    Refactor TransactionController to use 'store' method instead of 'create'

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth.bearer')->group(function () {
     Route::resource('users', \App\Http\Controllers\UserController::class)->only(['index', 'show', 'store']);
-    Route::resource('transactions', \App\Http\Controllers\TransactionController::class)->only(['index', 'create']);
+    Route::resource('transactions', \App\Http\Controllers\TransactionController::class)->only(['index', 'store']);
     Route::get('/transactions/account/{accountNumber}', [\App\Http\Controllers\TransactionController::class, 'getByAccountNumber']);
     Route::get('/me', [\App\Http\Controllers\AuthController::class, 'me']);
 });
