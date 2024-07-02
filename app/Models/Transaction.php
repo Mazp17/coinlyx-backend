ed35dd77fd70d91ba6289f87de7363df07bffc17
Miguel Zapata <zpmaepp@gmail.com>
2024-07-02 14:01:12 -0500

    feat: register activity log in transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,15 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Transaction extends Model
 {
     use HasFactory;
+    use LogsActivity;
 
     protected $fillable = ['account_id', 'amount', 'type'];
 
     public function account()
     {
         return $this->belongsTo(Account::class);
+    }
+
+    /**
+     * Get the options for activity logging.
+     *
+     * @return \Spatie\Activitylog\LogOptions
+     */
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['account_id', 'amount', 'type'])
+            ->logOnlyDirty()
+            ->useLogName('transaction');
     }
 }
